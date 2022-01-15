89e114c289f4e31c8a032e4c320aa0d78376f5aa
GGSMNandathilaka <ggsmnandathilaka@gmail.com>
2022-01-15 22:49:26 +0530

    Redux devtools integration

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 /**
  * Using redux default store
  */
-// import store from "./store";
-// import { bugAdded, bugRemoved, bugResolved } from "./actions";
-
-// // console.log(store.getState());
-
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store changed", store.getState());
-// });
-
-// store.dispatch(bugAdded("Bug 1"));
-// store.dispatch(bugResolved(1));
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
 // console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed", store.getState());
+});
+
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugAdded("Bug 2"));
+store.dispatch(bugAdded("Bug 3"));
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
 
 /**
  * Using a custom redux store
  */
-import store from "./customStore";
-import * as actions from "./actions";
+// import store from "./customStore";
+// import * as actions from "./actions";
 
-store.subscribe(() => {
-  console.log("Store changed!!", store.getState());
-});
+// store.subscribe(() => {
+//   console.log("Store changed!!", store.getState());
+// });
 
-store.dispatch(actions.bugAdded("Bug 1"));
+// store.dispatch(actions.bugAdded("Bug 1"));
 // console.log(store.getState());
