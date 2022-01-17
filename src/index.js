8a4791da8d6a20344708c63c689c66eebe3a457d
GGSMNandathilaka <ggsmnandathilaka@gmail.com>
2022-01-17 10:38:06 +0530

    Selectors & memoization, enhance project structure

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
  * Using redux default store
  */
 import configureStore from "./store/configureStore";
-import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import {
+  bugAdded,
+  bugRemoved,
+  bugResolved,
+  bugAssignedToUser,
+  getUnresolvedBugs,
+  getBugsByUser,
+} from "./store/bugs";
 import { projectAdded } from "./store/projects";
+import { userAdded } from "./store/users";
 
 const store = configureStore();
 
@@ -11,15 +19,28 @@ const unsubscribe = store.subscribe(() => {
   console.log("Store changed", store.getState());
 });
 
+store.dispatch(userAdded({ name: "John Doe" }));
+store.dispatch(userAdded({ name: "Mark Doe" }));
+store.dispatch(userAdded({ name: "Jessy Doe" }));
 store.dispatch(bugAdded({ description: "Bug 1" }));
 store.dispatch(bugAdded({ description: "Bug 2" }));
 store.dispatch(bugAdded({ description: "Bug 3" }));
+store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+store.dispatch(bugAssignedToUser({ bugId: 2, userId: 2 }));
+store.dispatch(bugAssignedToUser({ bugId: 3, userId: 3 }));
 store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(bugRemoved({ id: 1 }));
-
 store.dispatch(projectAdded({ name: "Project 1" }));
 
-console.log(store.getState());
+// const unresolvedBugs = getUnresolvedBugs(store.getState());
+// console.log("Unresolved bugs:", unresolvedBugs);
+
+const x = getUnresolvedBugs(store.getState());
+const y = getUnresolvedBugs(store.getState());
+
+console.log(x === y);
+
+const assignedBugs = getBugsByUser(store.getState(), 3);
+console.log(assignedBugs);
 
 /**
  * Using a custom redux store
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
 
 let lastId = 0;
 
@@ -22,8 +23,29 @@ const slice = createSlice({
       const index = bugs.findIndex((bug) => bug.id === action.payload.id);
       bugs[index].resolved = true;
     },
+    bugAssignedToUser: (bugs, action) => {
+      const { bugId, userId } = action.payload;
+      const index = bugs.findIndex((bug) => bug.id === bugId);
+      bugs[index].userId = userId;
+    },
   },
 });
 
-export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export const { bugAdded, bugRemoved, bugResolved, bugAssignedToUser } =
+  slice.actions;
 export default slice.reducer;
+
+// selector
+
+// Memoization (Cache state of a selector) = > Memory optimization
+// bugs (not changed) => get unresolved bugs from the cache
+
+export const getUnresolvedBugs = createSelector(
+  (state) => state.entities.bugs,
+  (bugs) => bugs.filter((bug) => !bug.resolved)
+);
+
+export const getBugsByUser = createSelector(
+  [(state) => state.entities.bugs, (state, userId) => userId],
+  (bugs, userId) => bugs.filter((bug) => bug.userId === userId)
+);
